01b660abfc5cb15a95fbde10028d2e28e9f78246
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-04-08 12:05:52 +0000

    fix the bug of omitting the first item in the array

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//Task: Transform this simple sorting algorithm into a unique sort. 
+// Task: Transform this simple sorting algorithm into a unique sort. 
 // It should not return any duplicate values in the sorted array.
 
-//input: [1,5,2,1] => output: [1,2,5]
-//input: [4,2,2,3,2,2,2] => output: [2,3,4]
+// input: [1,5,2,1] => output: [1,2,5]
+// input: [4,2,2,3,2,2,2] => output: [2,3,4]
 
 const uniqSort = function(arr) {
     const breadcrumbs = {};
@@ -18,4 +18,4 @@ const uniqSort = function(arr) {
     return result.sort((a, b) => a - b);
 };
 
-uniqSort([4,2,2,3,2,2,4,2]); // => [2,3,4]
+uniqSort([4,2,2,3,2,2,2]); // => [2,3,4]
